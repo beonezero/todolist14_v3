91d10d7bb4180bab202236e3a3d3c992e088a105
Neakhai Yauheni <beonezero@protonmail.com>
2023-09-04 15:00:18 +0300

    create thunk

---
+++ src/state/store.ts
@@ -0,0 +1,23 @@
+import { tasksReducer } from './tasks-reducer';
+import { todolistsReducer } from './todolists-reducer';
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux';
+import thunkMiddleware , {ThunkDispatch} from "redux-thunk";
+import {useDispatch} from "react-redux";
+
+// объединяя reducer-ы с помощью combineReducers,
+// мы задаём структуру нашего единственного объекта-состояния
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+// непосредственно создаём store
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware));
+// определить автоматически тип всего объекта состояния
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+type AppDispatchType = ThunkDispatch<AppRootStateType, unknown, AnyAction>
+export const useAppDispatch = useDispatch<AppDispatchType>
+
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// @ts-ignore
+window.store = store;
